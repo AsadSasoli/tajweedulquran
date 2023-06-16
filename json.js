6529109b3d34d17e05997686977ae4fe73912838
Umar_Baloch <asadsasoli8@gmail.com>
2023-06-16 17:48:20 +0500

    firsttoo

---
+++ json.js
@@ -0,0 +1,38 @@
+var data = [
+    {
+        naam:"Hadiths of the Prophet",
+        imgs:"https://i0.wp.com/academiamag.com/wp-content/uploads/2018/11/Hadith-about-Knowledge.jpg?resize=770%2C593&ssl=1",
+    },
+    {
+        naam:"Quran with Tajweed",
+        imgs:"https://cdn-gblnp.nitrocdn.com/ckHDmEwPmzNuYYSirDDoRUCcNbqqNPjc/assets/images/optimized/rev-992cb6a/wp-content/uploads/2021/11/syed-aoun-abbas-RwWh6HbkP-s-unsplash-min-min-1080x675.jpg",
+    },
+    {
+        naam:"Islamic Studies",
+        imgs:"https://onlinemehrabqurancenter.com/wp-content/uploads/2021/12/Islamic-Studies-Online.jpg",
+    },
+    {
+        naam:"Quran Memorization",
+        imgs:"https://alquranworld.com/wp-content/uploads/2021/05/quran-1.jpg",
+    },
+    {
+        naam:"Quran with Translation",
+        imgs:"https://i.pinimg.com/550x/26/71/8a/26718a999e0904de656bc668a674aaa3.jpg",
+    },
+    {
+        naam:"History of Islam",
+        imgs:"https://www.alim.org/img/history.jpg",
+    },
+]
+for (a=0; a<data.length; a++){
+    document.getElementById('cardes').innerHTML+=`
+    <div class="coursebx">
+            <div class="coursebximg">
+                <img src="${data[a].imgs}" alt="">
+            </div>
+            <h1>${data[a].naam}</h1>
+            <button class="coursesbxbtn"><a href="https://wa.me/+447588193140">More Details</a></button>
+        </div>
+    `
+}
+
---
+++ script.js
@@ -0,0 +1,16 @@
+// function burgur(){
+//     var navbtn = document.getElementById('navbtn')
+//     if(navbtn.style.right=="-110%"){
+//         navbtn.style.right='-0%'
+//     }
+    
+// }
+function mane(){
+    var man = document.getElementById('man')
+    // man.style.left="10px"
+    if ( man.style.right==="-110%") {
+        man.style.right = "0%"
+    }else{
+        man.style.right = "-110%"    
+    }
+}
